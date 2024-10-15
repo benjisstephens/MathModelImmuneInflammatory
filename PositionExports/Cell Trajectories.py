25b36dfad6a879cc8dec751dd8311d39dae2f5bb
Ben <stephensbenjamin01@gmail.com>
2024-10-15 14:57:22 +0100

    Position Exports, KDEs & 3 System PDE solver

--- PositionExports/Cell Trajectories.py
+++ PositionExports/Cell Trajectories.py
@@ -11,36 +11,54 @@ import matplotlib.pyplot as plt
 #%% INITIALISE SETTINGS
 
 # Start all paths to start at origin?
-normalise = False
+normalise = True  # Set to True if you want to normalize the paths to start at the origin
 
-#%%
+# Read the file names from 'all_data_set_names.txt'
+with open("all_data_set_names.txt", "r") as f:
+    data_files = f.read().splitlines()
 
-# Load the CSV file
-data = pd.read_csv('M1_DRC_CTRL_CCL19_50_I_pos_export.txt')
+#%% Process and plot each file individually
 
-# Group by 'CellID'
-groups = data.groupby('CellID')
-
-plt.figure(figsize=(10, 8))
-
-for cell_id, group in groups:
-
-    # Set normalise = False to show relative starting positions of cells
-    if normalise == False:
-        x_group = group['x(microns)'] 
-        y_group = group['y(microns)']
+for data_file in data_files:
+    # Load the CSV file
+    data = pd.read_csv(data_file)
     
-    # Set normalise = True to normalise each cell path to start at origin
-    if normalise == True:
-        x_group = group['x(microns)'] - group['x(microns)'].iloc[0]
-        y_group = group['y(microns)'] - group['y(microns)'].iloc[0]
-    
-    plt.plot(x_group, y_group)
+    # Group by 'CellID'
+    groups = data.groupby('CellID')
 
-plt.xlabel('x (microns)')
-plt.ylabel('y (microns)')
-plt.title('Cell Trajectories')
-plt.legend()
-plt.grid()
-plt.show()
+    plt.figure(figsize=(7, 6))
+
+    for cell_id, group in groups:
+
+        # Set normalise = False to show relative starting positions of cells
+        if normalise == False:
+            x_group = group['x(microns)']
+            y_group = group['y(microns)']
+
+        # Set normalise = True to normalize each cell path to start at origin
+        if normalise == True:
+            x_group = group['x(microns)'] - group['x(microns)'].iloc[0]
+            y_group = group['y(microns)'] - group['y(microns)'].iloc[0]
+
+        plt.plot(x_group, y_group, label=f'Cell {cell_id}')
+
+    # Set labels and title
+    plt.xlabel('x (microns)')
+    plt.ylabel('y (microns)')
+    
+    plt.xlim(-125, 125)  # Adjust as needed
+    plt.ylim(-125, 125)  # Adjust as needed
+    
+    # Remove '_pos_export.txt' from the file name for the plot title and save file name
+    title = data_file.replace('_pos_export.txt', '')
+    plt.title(f'Cell Trajectories for {title}')
+    
+    # Add grid
+    plt.grid()
+
+    # Save the plot as an image file
+    plt.savefig(f"{title}_Cell_Trajectories.png", dpi=300, bbox_inches='tight')
+
+    # Show the plot (optional, can be removed if only saving)
+    plt.show()
 
--- PositionExports/Kernal Density Estimation.py
+++ PositionExports/Kernal Density Estimation.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 16:51:57 2024
+
+@author: 44788
+"""
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -5,37 +12,89 @@ import matplotlib.pyplot as plt
 #%% INITIALISE SETTINGS
 
 # Start all paths to start at origin?
-normalise = True
+normalise = True  # True or False
+axis = 'x'  # 'x' or 'y'
+bandwidth = 2
 
-#%%
+# Read the file names from 'all_data_set_names.txt'
+with open("all_data_set_names.txt", "r") as f:
+    data_files = f.read().splitlines()
 
-# Load the CSV file
-data = pd.read_csv('M1_DRC_CTRL_CCL19_50_I_pos_export.txt')
+#%% Process and plot each file individually
 
-plt.figure(figsize=(10, 6))
+for data_file in data_files:
+    # Load the CSV file
+    data = pd.read_csv(data_file)
+    
+    # Remove '_pos_export.txt' from the file name
+    title = data_file.replace('_pos_export.txt', '')
+    output_file = f"{title}_{axis}_1D_KDE.png"  # File name for saving
 
-if normalise == True:
-    
-    # Group the data by 'CellID'
-    groups = data.groupby('CellID')
-    
-    # Create a new column for normalized y positions
-    data['y_normal'] = 0
-    
-    # Normalize the y values so that the first position starts at 0 for each cell
-    for cell_id, group in groups:
-        y_initial = group['y(microns)'].iloc[0]  # Initial y value for each cell
-        data.loc[group.index, 'y_normal'] = group['y(microns)'] - y_initial  # Subtract initial y
-    
-    # Retitle the data column as it was before editing
-    data['y(microns)'] = data['y_normal']
-    
-# Add labels and a title
-sns.kdeplot(data['y(microns)'], bw_adjust=1, fill=True)
-plt.xlabel('y (microns)')
-plt.ylabel('Density')
-plt.title('Kernel Density Estimate of y (microns) for Cell Positions')
+    if normalise:
+        
+        # Group the data by 'CellID'
+        groups = data.groupby('CellID')
 
-# Show the plot
-plt.show()
+        if axis == 'y':
+            # Ensure the y(microns) column is float before normalizing
+            data['y(microns)'] = data['y(microns)'].astype(float)
+
+            # Create a new column for normalized y positions, initialized with float type
+            data['y_normal'] = 0.0
+
+            # Normalize the y values so that the first position starts at 0 for each cell
+            for cell_id, group in groups:
+                y_initial = group['y(microns)'].iloc[0]  # Initial y value for each cell
+                data.loc[group.index, 'y_normal'] = (group['y(microns)'] - y_initial).astype(float)  # Subtract initial y
+
+            # Use normalized y values
+            data['y(microns)'] = data['y_normal']
+
+        elif axis == 'x':
+            # Ensure the x(microns) column is float before normalizing
+            data['x(microns)'] = data['x(microns)'].astype(float)
+
+            # Create a new column for normalized x positions, initialized with float type
+            data['x_normal'] = 0.0
+
+            # Normalize the x values so that the first position starts at 0 for each cell
+            for cell_id, group in groups:
+                x_initial = group['x(microns)'].iloc[0]  # Initial x value for each cell
+                data.loc[group.index, 'x_normal'] = (group['x(microns)'] - x_initial).astype(float)  # Subtract initial x
+
+            # Use normalized x values
+            data['x(microns)'] = data['x_normal']
+
+    # Plot individual dataset
+    plt.figure(figsize=(7, 6))
+
+    if axis == 'y':
+        sns.kdeplot(data['y(microns)'], bw_adjust=bandwidth, fill=True)
+        plt.xlabel('y (microns)')
+        plt.title(f'Kernel Density Estimate of y (microns) for {title}')
+        
+        # # Adjust limits of data so that 0 is central
+        # y_min = data['y(microns)'].min()
+        # y_max = data['y(microns)'].max()
+        # limit = max(abs(y_min), abs(y_max))
+        
+        
+    elif axis == 'x':
+        sns.kdeplot(data['x(microns)'], bw_adjust=bandwidth, fill=True)
+        plt.xlabel('x (microns)')
+        plt.title(f'Kernel Density Estimate of x (microns) for {title}')
+        
+        # # Adjust limits of data so that 0 is central
+        # x_min = data['x(microns)'].min()
+        # x_max = data['x(microns)'].max()
+        # limit = max(abs(x_min), abs(x_max))
+       
+    plt.xlim(-100, 100)  # Symmetrical limits around 0
+    plt.ylim(0,0.09)
+    plt.ylabel('Density')
+    
+    # Save the plot to a PNG file
+    plt.savefig(output_file, dpi=300, bbox_inches='tight')  # Save with high quality
+     
+    plt.show()
 
